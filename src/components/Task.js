8e1fade004f20ffe2153d8ca5ed07ba13ab4c96e
aliasgs <syd.1991@gmail.com>
2022-08-19 18:22:21 +0200

    bg white

--- src/components/Task.js
+++ src/components/Task.js
@@ -29,7 +29,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           readOnly={true}
           name="title"
           placeholder="Input title"
-          style={{ background: 'red' }}
+          style={{ background: 'white' }}
         />
       </label>
 
