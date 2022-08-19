4bce006ebaad2e8b67f1b4f44f645fe62983b325
aliasgs <syd.1991@gmail.com>
2022-08-19 14:02:07 +0200

    fix code

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,5 @@
 module.exports = {
-- stories: [
--   '../src/**/*.stories.mdx',
--   '../src/**/*.stories.@(js|jsx|ts|tsx)'
-- ],
-+ stories: ['../src/components/**/*.stories.js'],
+  stories: ['../src/components/**/*.stories.js'],
   staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
